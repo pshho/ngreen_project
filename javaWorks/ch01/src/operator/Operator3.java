f0bdf1c129dc7f41ac9a7620b6fd20f56df22076
pshho <awdr498@gmail.com>
2023-05-16 14:56:20 +0900

    v18

---
+++ javaWorks/ch01/src/operator/Operator3.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class Operator3 {
+
+	public static void main(String[] args) {
+		// 조건 연산자 - 비교문 ? 참 : 거짓
+		boolean boo1 = (5 > 3) ? true : false;
+		
+		System.out.println(boo1);
+	}
+
+}
