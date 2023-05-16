89a5d071d38490de3487fcf899773df7fedd7f95
pshho <awdr498@gmail.com>
2023-05-16 13:17:30 +0900

    v18

--- pyWorks/연습장.py
+++ pyWorks/연습장.py
@@ -1,5 +1,15 @@
+import random
+
+'''
 text = input('입력하세요 : '
              ''
              ''
              '')
-print(text)
+# print(text)
+'''
+words = ['dog', 'cat', 'monkey', 'chicken', 'frog', 'horse']
+word = 'chicken'
+guessed_letters = 'c'
+print(guessed_letters.find(word))
+
+
