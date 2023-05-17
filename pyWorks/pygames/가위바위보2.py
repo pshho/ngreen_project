94d44385cff9235b5291d1186caa2b04b9c444b1
pshho <awdr498@gmail.com>
2023-05-17 20:30:42 +0900

    v23

--- pyWorks/pygames/가위바위보2.py
+++ pyWorks/pygames/가위바위보2.py
@@ -4,21 +4,14 @@ import random
 def play():
     print("♠ 가위 바위 보 게임 ♠")
     가위바위보 = [['가위', '찍', '2'], ['묵', '바위', 'rock'], ['보자기', 'b', '보']]
-    가위바위보2 = []
-
-    for i in range(len(가위바위보)):
-        for j in range(len(가위바위보)):
-            가위바위보[1]
-            가위바위보[2]
 
     you = input("가위, 바위, 보 중 하나 입력: ")
     num = random.randint(0, 2)
     com = random.choice(가위바위보[num])
 
     result = {0: '무승부', 1: '패', 2: '승'}
-    state = 0  # 상태 변수(0/1/2)
 
-    if you not in 가위바위보:
+    if you not in [rps for row in 가위바위보 for rps in row]:
         print("잘못된 입력입니다. 다시 입력해주세요.")
     else:
         if you in 가위바위보[num]:
--- pyWorks/연습장.py
+++ pyWorks/연습장.py
@@ -6,7 +6,7 @@ text = input('입력하세요 : '
              ''
              '')
 # print(text)
-'''
+
 words = ['dog', 'cat', 'monkey', 'chicken', 'frog', 'horse']
 word = 'chicken'
 guessed_letters = 'c'
@@ -18,5 +18,8 @@ b.sort()
 print(a)
 print(b)
 print(guessed_letters.find(word))
+'''
 
+가위바위보 = [['가위', '찍', '2'], ['묵', '바위', 'rock'], ['보자기', 'b', '보']]
+x = [rps for row in 가위바위보 for rps in row]
 
